1b7db876d520f2fbad28e1060ce23dde4d083c40
PraveenElango <praveenelango98@gmail.com>
2020-08-25 19:27:10 +0800

    Greets the user and exits when user inputs bye

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,16 @@
-public class Duke {
-    public static void main(String[] args) {
+import  java.util.Scanner;
+public class Duke{
+    public static void main(String[] args){
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
+        Scanner sc = new Scanner(System.in);
+        String op = sc.nextLine();
+        while (!op.equals("bye")) {
+            System.out.println(op);
+            op = sc.nextLine();
+        }
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
+
 
